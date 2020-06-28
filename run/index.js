435841ae99e218c11e2f12462c810820eade7560
juliocabrera820 <juliocabrera820@gmail.com>
2020-06-28 13:33:50 -0500

    iterar arreglo

---
+++ run/index.js
@@ -0,0 +1,2 @@
+const lenguajes = ["ruby", "java", "javascript", "elixir"];
+lenguajes.forEach((lenguaje) => console.log(lenguaje));
---
+++ run/index.ts
@@ -0,0 +1,2 @@
+const langs: Array<string> = ["ruby", "java", "javascript", "elixir"];
+langs.forEach((lenguaje) => console.log(lenguaje));
